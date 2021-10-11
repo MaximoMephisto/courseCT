6f2ece08347ee31f1dddb17938482ecc0255dfa2
MaximoMephisto <sorondomaximo1@gmail.com>
2021-10-11 12:58:40 -0300

    Cambios

--- course/lenguaje.php
+++ course/lenguaje.php
@@ -22,37 +22,41 @@ if (!isset($_SESSION['usuario'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CT</title>
+    <link rel="icon" type="image/png" href="../css/images/CT-logo.png">
     <link rel="stylesheet" href="../css/diseno.css">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="../css/fontawesome/css/fontawesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/ff041f84fb.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body id="body-lenguaje">
-    <div class="col-12 col-sm-12 text-center mt-3">
+    <div class="col-12 col-sm-12 text-center mt-5">
         <div class="bienvenida">
-            <?php echo "¡Welcome! " .$_SESSION['usuario'] ."<br> Select your language"?>
+            <?php echo "¡Welcome! " . '<a class="user-name">'  . $_SESSION['usuario'] . '</a>' . "<br> Select your language"?>
         </div>
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-12 col-sm-6 mt-3">
+            <div class="col-12 col-sm-6 mt-5">
                 <div class="card">
-                    <img class="card-img-top" src="../css/images/ingles.png" alt="Ingles">
+                    <div class="card-header text-center">
+                        <img class="card-img-top" src="../css/images/ingles.png" alt="Ingles">
+                    </div>
                     <div class="card-body text-center">
                         <h5 class="card-title">English</h5>
-                        <a href="course" class="btn btn-dark">Go to study</a>
+                        <a href="course" class="btn btn-outline-primary">Go to study</a>
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-sm-6 mt-3 mb-3">
+            <div class="col-12 col-sm-6 mt-5 mb-3">
                 <div class="card">
-                    <img class="card-img-top" src="../css/images/español.png" alt="Español">
+                    <div class="card-header text-center">
+                        <img class="card-img-top" src="../css/images/español.png" alt="Español">
+                    </div>
                     <div class="card-body text-center">
                         <h5 class="card-title">Español</h5>
-                        <a href="course_es" class="btn btn-dark">¡A estudiar!</a>
+                        <a href="course_es" class="btn btn-outline-primary">¡A estudiar!</a>
                     </div>
                 </div>
             </div>
